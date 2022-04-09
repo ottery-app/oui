abc02997fbdd78ad32a2bb3770eea5bf095b0a4d
lewibs <benjaminsl2000@gmail.com>
2022-04-09 15:56:10 -0400

    added supported option and also personal json to menu input

--- input/Menu.js
+++ input/Menu.js
@@ -7,25 +7,43 @@ import Select from '@mui/material/Select';
 import axios from 'axios';
 
 export default function Menu(props) {
-    const [content, setContent] = React.useState((props.content) ? props.content : []);
+    const [content, setContent] = React.useState([]);
 
     React.useEffect(() => {
+
+        let link = props.link;
+
         switch (props.type) {
             case "countries":
-                axios.get('https://raw.githubusercontent.com/ottery-app/global-data/main/geographic/supportedCountries.json')
-                .then (response => {
-                    setContent(response.data.map(country => country.name));
-                });
+                link = "https://raw.githubusercontent.com/ottery-app/global-data/main/geographic/countries.json";
                 break;
             case "states":
-                axios.get('https://raw.githubusercontent.com/ottery-app/global-data/main/geographic/US/states.json')
-                .then (response => {
-                    setContent(response.data.map(state => state.name));
-                });
+                link = 'https://raw.githubusercontent.com/ottery-app/global-data/main/geographic/US/states.json';
                 break;
             default:
-                throw new Error("Menu type not supported");
+                if (props.link === undefined) {
+                    setContent(props.content);
+                }
             }
+
+        
+        if (link) {
+            link = (props.supported) ? link.replace(".json", ".supported.json") : link;
+
+            axios.get(link)
+            .then (response => {
+                if (typeof response.data === "string") {
+                    response.data = JSON.parse(response.data);
+                }
+
+                setContent(response.data.map(val => val.name));
+            })
+            .catch(error => {
+                console.error("something went wrong. 1. check that the link you provided was correct. 2. the link should be to a json array with this format: [{\"name\":\"val\"}]", error);
+            });
+        }
+
+
     }, []);
 
     return (
